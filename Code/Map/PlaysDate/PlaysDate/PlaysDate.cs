d6b206972d5e9711b8ac64aa3260287cb3a517d3
Boksic <boksic.rodrigo@ynov.com>
2015-11-19 11:48:31 +0100

    Première version Map
    Première version de la fonction Map de l’application

---
+++ Code/Map/PlaysDate/PlaysDate/PlaysDate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace PlaysDate
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			var tabs = new TabbedPage ();
+
+			// demonstrates the map control with zooming and map-types
+			tabs.Children.Add (new MapPage {Title = "Map/Zoom", Icon = "glyphish_74_location.png"});
+
+			// demonstrates the map control with zooming and map-types
+			tabs.Children.Add (new PinPage {Title = "Pins", Icon = "glyphish_07_map_marker.png"});
+
+			// demonstrates the Geocoder class
+			tabs.Children.Add (new GeocoderPage {Title = "Geocode", Icon = "glyphish_13_target.png"});
+
+			// opens the platform's native Map app
+			tabs.Children.Add (new MapAppPage {Title = "Map App", Icon = "glyphish_103_map.png"});
+
+			MainPage = tabs;
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ Code/Map/PlaysDate/iOS/obj/iPhoneSimulator/Debug/__XamarinInsightsApiKeyClass.ide.cs
@@ -0,0 +1,7 @@
+namespace PlaysDate.iOS
+{
+	class XamarinInsights
+	{
+		public const string ApiKey = "37f3fcdda29073773c7f1ed3e4c744271d5deed4";
+	}
+}
