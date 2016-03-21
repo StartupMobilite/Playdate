783520b094418be65e93405af3048c28fdb21ee0
Boksic <boksic.rodrigo@ynov.com>
2016-03-21 23:58:02 +0100

    Ajout Connexion
    - Mise a jour sur la gestion connexion/inscription
    - Ajout de la Connexion à l’application

---
+++ Code/Connexion/Connexion/BaseDeDonnee/PersonneBaseDeDonnees.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using SQLite.Net;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Connexion
+{
+	public class PersonneBaseDeDonnees
+	{
+		private SQLiteConnection _connection;
+
+		public PersonneBaseDeDonnees ()
+		{
+			_connection = DependencyService.Get<ISQLite> ().GetConnection ();
+			_connection.CreateTable<PersonBD> ();
+		}
+
+		public IEnumerable<PersonBD> GetDatas() 
+		{
+			return (from t in _connection.Table<PersonBD> ()
+				select t).ToList ();
+		}
+
+		public PersonBD GetData(string pseudo) 
+		{
+			return _connection.Table<PersonBD> ().FirstOrDefault (t => t.PersonPseudo == pseudo);
+		}
+
+		public PersonBD PersonExistForConnexion(string pseudo, string mdp)
+		{
+			return _connection.Table<PersonBD> ().FirstOrDefault (t => (t.PersonPseudo == pseudo && t.PersonMotDePasse == mdp));
+		}
+
+		public void DeleteData(string pseudo) 
+		{
+			var retour = _connection.Table<PersonBD> ().FirstOrDefault (t => t.PersonPseudo.Equals(pseudo));
+
+			if(retour != null)
+			{
+				_connection.Delete<PersonBD> (retour.ID);
+			}
+		}
+
+		public void DeleteDatas()
+		{
+			_connection.DeleteAll<PersonBD> ();
+		}
+
+		public void AddData(string nom, string prenom, string pseudo, string email, string mdp) 
+		{
+			var newData = new PersonBD {
+				PersonNom = nom,
+				PersonPrenom = prenom,
+				PersonPseudo = pseudo,
+				PersonMail = email,
+				PersonMotDePasse = mdp
+			};
+
+			_connection.Insert (newData);
+		}
+	}
+}
--- Code/Connexion/Connexion/ConnexionPage.xaml.cs
+++ Code/Connexion/Connexion/ConnexionPage.xaml.cs
@@ -14,6 +14,8 @@ namespace Connexion
 {
 	public partial class ConnexionPage : TabbedPage
 	{
+		private PersonneBaseDeDonnees _database;
+
 		private readonly TaskScheduler _scheduler = TaskScheduler.FromCurrentSynchronizationContext();
 
 		private IMediaPicker mediaPicker;
@@ -51,7 +53,6 @@ namespace Connexion
 
 		private async Task SelectPicture()
 		{
-
 			mediaPicker = DependencyService.Get<IMediaPicker>();
 
 			Poster.Source = null;
@@ -105,45 +106,64 @@ namespace Connexion
 
 		public void OnInscriptionButtonClicked(object sender, EventArgs args)
 		{
+			_database = new PersonneBaseDeDonnees ();
+
+			PersonBD pseudoExist = _database.GetData (userNameEntry.Text);
+
 			//Gestion pour l'inscription
 			Regex mailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
 
 			Match mailMatch	= mailRegex.Match (emailEntry.Text);
 
-			//Si tous les champs ne sont pas rempli, on revoie un message d'erreur à l'utilisateur
+			//Si tous les champs ne sont pas rempli, on renvoie un message d'erreur à l'utilisateur
 			if (firstNameEntry.Text == null || lastNameEntry.Text == null || userNameEntry.Text == null || emailEntry.Text == null || passwordEntry.Text == null || VerifyPasswordEntry.Text == null || firstNameEntry.Text == "" || lastNameEntry.Text == "" || userNameEntry.Text == "" || emailEntry.Text == "" || passwordEntry.Text == "" || VerifyPasswordEntry.Text == "") 
 			{
 				erreurInscriptionMessageLabel.Text = "Vous devez remplir tous les champs !!!";
 			}
-			//Si l'addresse mail rentrer n'est pas valide, on revoie un message d'erreur à l'utilisateur
+			//Si l'addresse mail rentrer n'est pas valide, on renvoie un message d'erreur à l'utilisateur
 			else if (!mailMatch.Success) 
 			{
 				erreurInscriptionMessageLabel.Text = "Adresse mail non valide !!!";
 			}
-			//Si le mot de passe et la verification de mot de passe ne corresponde pas, on revoie un message d'erreur à l'utilisateur
+			//Si le mot de passe et la verification de mot de passe ne corresponde pas, on renvoie un message d'erreur à l'utilisateur
 			else if (passwordEntry.Text != VerifyPasswordEntry.Text)
 			{
 				erreurInscriptionMessageLabel.Text = "Les deux mot de passe ne sont pas les mêmes !!!";
 			}
+			//Si le pseudo existe deja, on renvoie un message d'erreur à l'utilisateur
+			else if (pseudoExist != null)
+			{
+				erreurInscriptionMessageLabel.Text = "Ce pseudo existe déjà !";
+			}
 			//Sinon on effectue l'inscription dans la base de données
 			else 
 			{
-				erreurInscriptionMessageLabel.Text = "Done";
+				_database.AddData (firstNameEntry.Text, lastNameEntry.Text, userNameEntry.Text, emailEntry.Text, passwordEntry.Text);
+
+				App.Current.MainPage = new ConnexionPage ();
 			}
 		}
 
 		public void OnConnexionButtonClicked(object sender, EventArgs args)
 		{
 			//Gestion pour la connexion
+			_database = new PersonneBaseDeDonnees ();
 
-			//Si tous les champs ne sont pas rempli, on revoie un message d'erreur à l'utilisateur
+			PersonBD personExistForConnection = _database.PersonExistForConnexion(userNameConnexionEntry.Text, passwordConnexionEntry.Text);
+
+			//Si tous les champs ne sont pas rempli, on renvoie un message d'erreur à l'utilisateur
 			if (userNameConnexionEntry.Text == null || passwordConnexionEntry.Text == null || userNameConnexionEntry.Text == "" || passwordConnexionEntry.Text == "") 
 			{
 				erreurConnexionMessageLabel.Text = "Vous devez remplir tous les champs !!!";
 			} 
+			//Si le pseudo et/ou le mot de passe nexist(nt) pas dans la base de données, on revoie un message d'erreur à l'utilisateur
+			else if (personExistForConnection == null)
+			{
+				erreurConnexionMessageLabel.Text = "Le pseudo et/ou le mot de passe n'existe(nt) pas !!!";
+			}
 			else 
 			{
-				erreurConnexionMessageLabel.Text = "Done";
+				App.Current.MainPage = new ProfilePage ();
 			}
 		}
 	}
